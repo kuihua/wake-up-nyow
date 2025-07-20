0339deb63d69101f223a6ff404718038fdf2e68b
Jyl7hf <lijillian@hotmail.com>
2025-07-20 01:33:23 -0700

    fan switch on sprite

--- Assets/Scripts/CeilingFanSwitch.cs
+++ Assets/Scripts/CeilingFanSwitch.cs
@@ -4,11 +4,13 @@ public class CeilingFanSwitch : Interactable
 {
     [SerializeField] private CeilingFan fan;
     [SerializeField] private AudioClip switchOnClip, switchOffClip;
+    [SerializeField] private Sprite switchOffSprite, switchOnSprite;
+    private SpriteRenderer sr;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -20,10 +22,12 @@ public class CeilingFanSwitch : Interactable
     public override void interact() {
         if(fan.GetComponent<Collider2D>().enabled) {
             SoundFXManager.instance.PlaySoundFXClip(switchOffClip, transform, 1f);
+            sr.sprite = switchOffSprite;
             fan.turnOff();
         }
         else{
             SoundFXManager.instance.PlaySoundFXClip(switchOnClip, transform, 1f);
+            sr.sprite = switchOnSprite;
             fan.turnOn();
         }
         Debug.Log("switch");
